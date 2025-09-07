a450b7ef83a523ee40bdc84b2a8a697107c4884d
Shashi Kant Pandey <83300694+8349712796@users.noreply.github.com>
2025-09-07 11:11:24 +0530

    Update Int_database.sql

--- scripts/Int_database.sql
+++ scripts/Int_database.sql
@@ -1,1 +1,42 @@
 
+/*
+Create Database & Schemas
+Script purpose: 
+This script create a new database named 'Datawarehouse' after checking if it already exist.
+if the databse exists, it is dropped & recreated. Additionally script setup three schemas
+within the database: 'Bronze', 'Silver', 'Gold'
+
+Warning:
+Running this script will drop your database permanently if it exists.
+All the data in the database wil permanently will be deleted. Proceed with caution
+and ensure you have proper backups before running this script.
+*/
+
+
+Use master;
+Go
+
+--Drop & recreate Datawarehouse
+If exists (Select 1 From sys.databases where name = 'Datawarehouse')
+Begin
+	Alter Database Datawarehouse Set Single_User With Rollback Immediate;
+	Drop Database Datawarehouse
+End;
+Go
+
+--Create the database 'Datawarehouse'
+Create Database Datawarehouse;
+Go
+
+Use Datawarehouse
+Go
+
+-- Create Schemas
+Create Schema Bronze;
+Go
+
+Create Schema Silver;
+Go
+
+Create Schema Gold;
+Go
